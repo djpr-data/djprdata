091313831824ec6de29fe10f9cff4f28a304e7c9
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2022-03-02 16:55:12 +1100

    first basic shell draft for function

---
+++ R/read_eftsl_by_postcode.R
@@ -0,0 +1,21 @@
+#' @title Read equivalent full-time student load data by postcode
+#'
+#' @description data on international and domestic higher education students by postcode of term residence
+#' @note EFTSL (Equivalent full-time student load)
+#' @note data comes from DESE (HEIMSHELP)
+#' @source: https://heimshelp.dese.gov.au/
+#' Replaces the data import component of {https://github.com/djpr-data/osd-nous-dashboard/blob/main/processing/eftsl-by-postcode.R}
+
+read_eftsl_by_postcode <- function(path = tempdir(), delete = TRUE) {
+
+
+
+
+
+
+
+
+
+
+
+}
