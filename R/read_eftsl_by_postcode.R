9b27a303aac64ca165a1cf45565e40d9b1ef10d0
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2022-03-04 14:15:52 +1100

    added tidyr and dplyr in header and moved comments to header.

--- R/read_eftsl_by_postcode.R
+++ R/read_eftsl_by_postcode.R
@@ -3,19 +3,15 @@
 #' @description data on international and domestic higher education students by postcode of term residence
 #' @note EFTSL (Equivalent full-time student load)
 #' @note data comes from DESE (HEIMSHELP)
-#' @source: https://heimshelp.dese.gov.au/
+#' @source \url{https://heimshelp.dese.gov.au}
+#' @note data is not publicly available, but we may be able to request access via https://www.dese.gov.au/higher-education-statistics/resources/higher-education-data-request-form
+#' @note there are data publicly available, but only by university, not by postcode: https://www.dese.gov.au/higher-education-statistics/resources/student-load-pivot-table
 #' Replaces the data import component of {https://github.com/djpr-data/osd-nous-dashboard/blob/main/processing/eftsl-by-postcode.R}
+#' @import dplyr
+#' @import tidyr
 
 read_eftsl_by_postcode <- function(path = tempdir(), delete = TRUE) {
 
 message("Data on equivalent full-time student load by postcode is not publicly available")
-
-# data is not publicly available, but we may be able to request access via:
-# https://www.dese.gov.au/higher-education-statistics/resources/higher-education-data-request-form
-
-
-# there are data publicly available, but only by university, not by postcode:
-# https://www.dese.gov.au/higher-education-statistics/resources/student-load-pivot-table
-
 
 }
